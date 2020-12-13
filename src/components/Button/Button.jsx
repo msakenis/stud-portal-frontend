338d57b98c5b24c72bc9ec4f5f47becbf823f402
MindaugasSak <m.sakenis@gmail.com>
2020-12-13 18:24:18 +0200

    button type changed to dynamic and style

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import * as S from './Button.style';
-function Button({ children, handleClick, color }) {
+function Button({ children, handleClick, type }) {
   return (
-    <S.Button onClick={handleClick} color={color}>
+    <S.Button onClick={handleClick} type={type}>
       {children}
     </S.Button>
   );
--- src/components/Button/Button.style.js
+++ src/components/Button/Button.style.js
@@ -1,26 +1,30 @@
 import styled from 'styled-components';
 
 export const Button = styled.button`
-  background: ${(props) =>
-    props.color === 'primary'
-      ? props.theme.primary.background
-      : props.theme.secondary.background};
-  color: ${(props) =>
-    props.color === 'primary'
-      ? props.theme.primary.color
-      : props.theme.secondary.color};
+  background: linear-gradient(
+    150deg,
+    rgba(75, 131, 184, 0.9584034297312675) 14%,
+    rgba(62, 108, 152, 1) 37%,
+    rgba(47, 93, 135, 0.8743698162858894) 61%,
+    rgba(23, 59, 92, 0.9107843821122199) 77%,
+    rgba(19, 51, 80, 0.9135855025603992) 90%
+  );
+  color: #fff;
   border: none;
   border-radius: 5px;
-  padding: 0.5em 2em;
+  padding: 1em 2em;
+  font-weight: 500;
+  font-size: 1.1em;
+  width: 40%;
   cursor: pointer;
   &&:hover {
-    color: ${(props) =>
-      props.color === 'primary'
-        ? props.theme.primaryHover.color
-        : props.theme.secondaryHover.color};
-    background: ${(props) =>
-      props.color === 'primary'
-        ? props.theme.primaryHover.background
-        : props.theme.secondaryHover.background};
+    background: linear-gradient(
+      150deg,
+      rgba(94, 142, 187, 0.8099440459777661) 14%,
+      rgba(91, 151, 207, 1) 36%,
+      rgba(45, 108, 166, 0.8743698162858894) 56%,
+      rgba(62, 119, 171, 0.9107843821122199) 69%,
+      rgba(67, 133, 195, 0.9135855025603992) 97%
+    );
   }
 `;
