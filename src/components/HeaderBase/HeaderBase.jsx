1eb3ed6c46ed214e21546a7f2321a81a504dd39f
MindaugasSak <m.sakenis@gmail.com>
2020-12-13 18:28:00 +0200

    Header created

--- src/components/HeaderBase/HeaderBase.jsx
+++ src/components/HeaderBase/HeaderBase.jsx
@@ -1,13 +1,40 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { HighlightIdContext } from '../../contexts/HighlightId.context';
 import * as S from './HeaderBase.style';
-import logoImg from '../../assets/img/React-logo-1.png';
+import { IconNav } from '../';
 function HeaderBase() {
+  const selectedId = useContext(HighlightIdContext);
+  const nav = [
+    { id: 1, name: 'Home', to: '/', iconClass: 'fas fa-home' },
+    {
+      id: 2,
+      name: 'Courses',
+      to: '/courses',
+      iconClass: 'fas fa-graduation-cap',
+    },
+    {
+      id: 3,
+      name: 'Students',
+      to: '/students',
+      iconClass: 'fas fa-user-graduate',
+    },
+  ];
+
   return (
     <S.Header>
-      <S.Logo src={logoImg} alt="logo" />
+      <S.LogoText>CodeOp</S.LogoText>
       <S.Actions>
-        <S.StyledLink to="/">Home</S.StyledLink>
-        <S.StyledLink to="/about">About</S.StyledLink>
+        {nav.map((link) => (
+          <S.StyledLink
+            highlighted={
+              selectedId.selectedId === link.id ? 'marked' : undefined
+            }
+            key={link.id}
+            to={link.to}
+          >
+            <IconNav className={link.iconClass} /> {link.name}
+          </S.StyledLink>
+        ))}
       </S.Actions>
     </S.Header>
   );
--- src/components/HeaderBase/HeaderBase.style.js
+++ src/components/HeaderBase/HeaderBase.style.js
@@ -1,27 +1,42 @@
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
-export const Logo = styled.img`
-  max-height: 40px;
-  max-width: 100%;
-`;
 export const Header = styled.header`
+  width: 20%;
+  min-height: 100%;
   display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 1em;
+  flex-direction: column;
+  background-color: ${(props) => props.theme.secondary.color};
 `;
 
-export const Actions = styled.nav``;
+export const Actions = styled.nav`
+  display: flex;
+  flex-direction: column;
+`;
 
 export const StyledLink = styled(Link)`
-  color: #444;
-  font-size: 0.8em;
+  color: #fff;
+  font-size: 1.2em;
   text-decoration: none;
+  padding: 2em 0;
+  padding-left: 40px;
+  cursor: pointer;
+  background: ${(props) =>
+    props.highlighted === 'marked'
+      ? props.theme.secondaryHover.color
+      : 'unset'};
   &:hover {
-    text-decoration: underline;
+    background: ${(props) => props.theme.secondaryHover.color};
   }
-  &:not(:first-child) {
-    margin-left: 20px;
+  & i {
+    padding-right: 5px;
   }
+`;
+
+export const LogoText = styled.h1`
+  color: #fff;
+  margin: 0;
+  padding: 1em 0;
+  padding-left: 40px;
+  border-bottom: 2px solid #999;
 `;
