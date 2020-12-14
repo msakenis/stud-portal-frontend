f07571a5d289688a713a5f40f9e91edbe637f8f6
MindaugasSak <m.sakenis@gmail.com>
2020-12-14 23:09:26 +0200

    icon comp created

---
+++ src/components/IconInput/IconInput.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import * as S from './IconInput.style';
+function IconInput({ className }) {
+  return <S.StyledIcon className={className}></S.StyledIcon>;
+}
+
+export default IconInput;
