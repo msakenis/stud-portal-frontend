6bdcb525417aa2cc696779e41638a34c747304fa
MindaugasSak <m.sakenis@gmail.com>
2020-12-14 23:14:13 +0200

    added all current pages to routes

--- src/Routes.js
+++ src/Routes.js
@@ -1,16 +1,22 @@
 import React, { Suspense, lazy } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { Loading } from './components';
+import { Loading, PrivateRoute } from './components';
 
 const Homelazy = lazy(() => import('./pages/Home/Home'));
 const Loginlazy = lazy(() => import('./pages/Login/Login'));
+const ViewStudentslazy = lazy(() =>
+  import('./pages/ViewStudents/ViewStudents')
+);
+const AddStudentslazy = lazy(() => import('./pages/AddStudent/AddStudent'));
 function Routes() {
   return (
     <Router>
       <Suspense fallback={<Loading />}>
         <Switch>
-          <Route exact path="/" component={Homelazy} />
           <Route exact path="/login" component={Loginlazy} />
+          <PrivateRoute exact path="/" component={Homelazy} />
+          <PrivateRoute exact path="/students" component={ViewStudentslazy} />
+          <PrivateRoute exact path="/add-student" component={AddStudentslazy} />
         </Switch>
       </Suspense>
     </Router>
