f8b36808d97234400e1db83fe85c14ca1edc1ee4
MindaugasSak <m.sakenis@gmail.com>
2020-12-14 23:13:15 +0200

    home just changed structure

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,22 +1,18 @@
-import React, { useContext } from 'react';
-import { HeaderBase, Wrapper, FlexDiv } from '../../components';
+import React, { useContext, useEffect } from 'react';
+import { MainWrapper } from '../../components';
 import { HighlightIdContext } from '../../contexts/HighlightId.context';
 
 function Home() {
   const selectedId = useContext(HighlightIdContext);
-  selectedId.setId(1);
+
+  useEffect(() => {
+    selectedId.setId(1);
+  }, [selectedId]);
 
   return (
-    <Wrapper
-      backgroundImg={
-        'https://pe-insights.com/wp-content/uploads/2020/08/Business-wallpaper1.jpg'
-      }
-    >
-      <FlexDiv>
-        <HeaderBase />
-        <h1>Content</h1>
-      </FlexDiv>
-    </Wrapper>
+    <MainWrapper>
+      <div>aaa</div>
+    </MainWrapper>
   );
 }
 
