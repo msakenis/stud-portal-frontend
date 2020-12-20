57d8805b39f2e98f7ca08d221ee479051d4c7a1f
MindaugasSak <m.sakenis@gmail.com>
2020-12-20 09:56:28 +0200

    search feature and total students count added

--- src/pages/ViewStudents/ViewStudents.jsx
+++ src/pages/ViewStudents/ViewStudents.jsx
@@ -16,8 +16,8 @@ function deleteStudent(
   setNotifType,
   error,
   token,
-  setData,
-  data
+  setFilteredData,
+  filteredData
 ) {
   error = false;
   fetch(`${process.env.REACT_APP_SERVER_URL}/DeleteStudent`, {
@@ -30,7 +30,7 @@ function deleteStudent(
   })
     .then((res) => {
       if (res.ok) {
-        setData(data.filter((item) => item.id !== id));
+        setFilteredData(filteredData.filter((item) => item.id !== id));
       } else {
         error = true;
         setNotifType('error');
@@ -52,6 +52,7 @@ function ViewStudents() {
   const selectedId = useContext(HighlightIdContext);
   const token = localStorage.getItem('token');
   const [data, setData] = useState();
+  const [filteredData, setFilteredData] = useState();
   const [error, setError] = useState(false);
   const [errorMessage, setErrorMessage] = useState();
   const [notifType, setNotifType] = useState();
@@ -68,15 +69,28 @@ function ViewStudents() {
       .then((res) => res.json())
       .then((data) => {
         setData(data);
+        setFilteredData(data);
       });
   }, [token, selectedId]);
 
   return (
     <MainWrapper
-      backgroundImg={
-        'https://pe-insights.com/wp-content/uploads/2020/08/Business-wallpaper1.jpg'
-      }
       addBtnTxt={'STUDENT'}
+      handleSearch={(e) => {
+        setFilteredData(
+          data.filter(
+            (item) =>
+              item.name.toLowerCase().includes(e.target.value.toLowerCase()) ||
+              item.surname
+                .toLowerCase()
+                .includes(e.target.value.toLowerCase()) ||
+              item.email.toLowerCase().includes(e.target.value.toLowerCase()) ||
+              item.phone.includes(e.target.value) ||
+              (item.title &&
+                item.title.toLowerCase().includes(e.target.value.toLowerCase()))
+          )
+        );
+      }}
     >
       <S.TableBox>
         <S.Title>Students</S.Title>
@@ -84,55 +98,64 @@ function ViewStudents() {
           <Notification message={errorMessage} type={notifType} />
         )}
         {data ? (
-          <S.Table>
-            <thead>
-              <tr>
-                <th>FULL NAME</th>
-                <th>PHONE</th>
-                <th>EMAIL</th>
-                <th>LAST MODIFIED</th>
-                <th>ACTION</th>
-              </tr>
-            </thead>
-            <tbody>
-              {data &&
-                data.map((row) => (
-                  <tr key={row.id}>
-                    <td>{row.name + ' ' + row.surname}</td>
-                    <td>{row.phone}</td>
-                    <td>{row.email}</td>
-                    <td>{row.last_modified.slice(0, 10)}</td>
-                    <td>
-                      <IconBtnsConfirm
-                        id={row.id}
-                        displayBtns={displayBtns}
-                        selectedId={selectedIcon}
-                        handleClick={() => {
-                          setDisplayBtns(true);
-                          setSelectedIcon(row.id);
-                        }}
-                        handleConfirm={() =>
-                          deleteStudent(
-                            selectedIcon,
-                            setError,
-                            setErrorMessage,
-                            setNotifType,
-                            error,
-                            token,
-                            setData,
-                            data
-                          )
-                        }
-                        handleClose={() => setDisplayBtns(false)}
-                        handleEdit={() =>
-                          history.push(`/edit-student/${row.id}`)
-                        }
-                      />
-                    </td>
-                  </tr>
-                ))}
-            </tbody>
-          </S.Table>
+          <S.FlexDiv>
+            <S.Table>
+              <thead>
+                <tr>
+                  <th>FULL NAME</th>
+                  <th>PHONE</th>
+                  <th>EMAIL</th>
+                  <th>LAST MODIFIED</th>
+                  <th>CURRENT COURSE</th>
+                  <th>ACTION</th>
+                </tr>
+              </thead>
+              <tbody>
+                {filteredData &&
+                  filteredData.map((row) => (
+                    <tr key={row.id}>
+                      <td>{row.name + ' ' + row.surname}</td>
+                      <td>{row.phone}</td>
+                      <td>{row.email}</td>
+                      <td>{row.last_modified.slice(0, 10)}</td>
+                      <td>{row.title ? row.title : 'Not Studing'}</td>
+                      <td>
+                        <IconBtnsConfirm
+                          id={row.id}
+                          displayBtns={displayBtns}
+                          selectedId={selectedIcon}
+                          handleClick={() => {
+                            setDisplayBtns(true);
+                            setSelectedIcon(row.id);
+                          }}
+                          handleConfirm={() =>
+                            deleteStudent(
+                              selectedIcon,
+                              setError,
+                              setErrorMessage,
+                              setNotifType,
+                              error,
+                              token,
+                              setFilteredData,
+                              filteredData
+                            )
+                          }
+                          handleClose={() => setDisplayBtns(false)}
+                          handleEdit={() =>
+                            history.push(`/edit-student/${row.id}`)
+                          }
+                        />
+                      </td>
+                    </tr>
+                  ))}
+              </tbody>
+            </S.Table>
+            {filteredData && (
+              <S.StudentsCount>
+                Total Students: {filteredData.length}
+              </S.StudentsCount>
+            )}
+          </S.FlexDiv>
         ) : (
           <Loading />
         )}
--- src/pages/ViewStudents/ViewStudents.style.js
+++ src/pages/ViewStudents/ViewStudents.style.js
@@ -2,6 +2,69 @@ import styled from 'styled-components';
 
 export const TableBox = styled.div`
   padding: 2em;
+  box-sizing: border-box;
+  max-height: 75vh;
+  overflow-y: auto;
+  @media (max-width: ${({ theme }) => theme.smallLaptop}) {
+    padding: 1.2em;
+  }
+  @media (max-width: ${({ theme }) => theme.tablet}) {
+    max-height: 88vh;
+  }
+  @media (max-width: ${({ theme }) => theme.mobile}) {
+    font-size: 1.4em;
+    && table,
+    thead,
+    tbody,
+    th,
+    td,
+    tr {
+      display: block;
+    }
+    && thead tr {
+      position: absolute;
+      top: -9999px;
+      left: -9999px;
+    }
+    && tr {
+      border: 1px solid #ccc;
+    }
+    && td {
+      /* Behave  like a "row" */
+      border: none;
+      border-bottom: 1px solid #eee;
+      position: relative;
+      padding-left: 50% !important;
+    }
+    && td:before {
+      /* Now like a table header */
+      position: absolute;
+      /* Top/left values mimic padding */
+      top: 6px;
+      left: 6px;
+      width: 45%;
+      padding-right: 10px;
+      white-space: nowrap;
+    }
+    && td:nth-of-type(1):before {
+      content: 'FULL NAME';
+    }
+    && td:nth-of-type(2):before {
+      content: 'PHONE';
+    }
+    && td:nth-of-type(3):before {
+      content: 'EMAIL';
+    }
+    && td:nth-of-type(4):before {
+      content: 'LAST MODIFIED';
+    }
+    && td:nth-of-type(5):before {
+      content: 'CURRENT COURSE';
+    }
+    && td:nth-of-type(6):before {
+      content: 'ACTION';
+    }
+  }
 `;
 export const Title = styled.h2`
   font-weight: 400;
@@ -34,5 +97,21 @@ export const Table = styled.table`
   && td:first-child {
     color: ${(props) => props.theme.secondary.color};
     font-weight: 500;
+  }
+`;
+
+export const FlexDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+`;
+export const StudentsCount = styled.div`
+  position: absolute;
+  bottom: 5%;
+  color: ${(props) => props.theme.primary.color};
+  font-size: 0.9em;
+  font-weight: 500;
+  @media (max-width: ${({ theme }) => theme.tablet}) {
+    bottom: 2%;
   }
 `;
