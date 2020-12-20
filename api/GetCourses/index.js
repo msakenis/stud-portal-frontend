1fa26537cfc071df9f26ea031c757dfc4c347564
MindaugasSak <m.sakenis@gmail.com>
2020-12-20 10:42:24 +0200

    api added for azure automation with static web app

---
+++ api/GetCourses/index.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken');
+const { sendContext } = require('../SharedCode/sendContext');
+const database = require('../SharedCode/db');
+
+module.exports = function (context, req) {
+  try {
+    // checks if logged in
+    const token = req.headers.authorization.split(' ')[1];
+    const decodedToken = jwt.verify(token, process.env.SECRETKEY);
+
+    database((db) =>
+      db.query(`SELECT * FROM courses`, (err, result) => {
+        if (err) {
+          sendContext(context, err, 400);
+        } else {
+          sendContext(context, result, 200);
+        }
+      })
+    );
+  } catch (err) {
+    sendContext(context, { msg: 'Please login to process this action!' }, 401);
+  }
+};
