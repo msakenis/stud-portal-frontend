acd492b91e01815e7454b67b0d0e3b555279ea15
MindaugasSak <m.sakenis@gmail.com>
2020-12-20 09:59:35 +0200

    view course page added

---
+++ src/pages/ViewCourses/ViewCourses.jsx
@@ -0,0 +1,170 @@
+import React, { useContext, useEffect, useState } from 'react';
+import {
+  MainWrapper,
+  Loading,
+  Notification,
+  IconBtnsConfirm,
+} from '../../components';
+import { HighlightIdContext } from '../../contexts/HighlightId.context';
+import * as S from './ViewCourses.style';
+import { useHistory } from 'react-router-dom';
+
+function deleteCourse(
+  id,
+  setError,
+  setErrorMessage,
+  setNotifType,
+  error,
+  token,
+  setFilteredData,
+  filteredData
+) {
+  error = false;
+  fetch(`${process.env.REACT_APP_SERVER_URL}/DeleteCourse`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: token,
+    },
+    body: JSON.stringify({ id: id }),
+  })
+    .then((res) => {
+      if (res.ok) {
+        setFilteredData(filteredData.filter((item) => item.id !== id));
+      } else {
+        error = true;
+        setNotifType('error');
+      }
+      return res.json();
+    })
+    .then((data) => {
+      setError(true);
+      setErrorMessage(data.msg || 'Error!');
+    })
+    .catch((err) => {
+      setError(true);
+      setNotifType('error');
+      setErrorMessage(err.message);
+    });
+}
+
+function ViewCourses() {
+  const selectedId = useContext(HighlightIdContext);
+  const token = localStorage.getItem('token');
+  const [data, setData] = useState();
+  const [filteredData, setFilteredData] = useState();
+  const [error, setError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState();
+  const [notifType, setNotifType] = useState();
+  const [displayBtns, setDisplayBtns] = useState(false);
+  const [selectedIcon, setSelectedIcon] = useState('');
+  const history = useHistory();
+  useEffect(() => {
+    selectedId.setId(2);
+    fetch(`${process.env.REACT_APP_SERVER_URL}/GetCourses`, {
+      headers: {
+        Authorization: token,
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data);
+        setFilteredData(data);
+      });
+  }, [token, selectedId]);
+
+  return (
+    <MainWrapper
+      addBtnTxt={'COURSE'}
+      handleSearch={(e) => {
+        setFilteredData(
+          data.filter(
+            (item) =>
+              item.title.toLowerCase().includes(e.target.value.toLowerCase()) ||
+              item.lecturer
+                .toLowerCase()
+                .includes(e.target.value.toLowerCase()) ||
+              item.description
+                .toLowerCase()
+                .includes(e.target.value.toLowerCase())
+          )
+        );
+      }}
+    >
+      <S.TableBox>
+        <S.Title>Courses</S.Title>
+        {error && notifType && (
+          <Notification message={errorMessage} type={notifType} />
+        )}
+        {data ? (
+          <S.FlexDiv>
+            <S.Table>
+              <thead>
+                <tr>
+                  <th>COURSE TITLE</th>
+                  <th>LECTURER</th>
+                  <th>DESCRIPTION</th>
+                  <th>STUDENTS IN COURSE</th>
+                  <th>CREATED</th>
+                  <th>ACTION</th>
+                </tr>
+              </thead>
+              <tbody>
+                {filteredData &&
+                  filteredData.map((row) => (
+                    <tr key={row.id}>
+                      <td>{row.title}</td>
+                      <td>{row.lecturer}</td>
+                      <td>{row.description}</td>
+                      <td>
+                        {row.student_count === 0
+                          ? 'No Students'
+                          : row.student_count}
+                      </td>
+                      <td>{row.reg_date.slice(0, 10)}</td>
+                      <td>
+                        <IconBtnsConfirm
+                          id={row.id}
+                          displayBtns={displayBtns}
+                          selectedId={selectedIcon}
+                          handleClick={() => {
+                            setDisplayBtns(true);
+                            setSelectedIcon(row.id);
+                          }}
+                          handleConfirm={() =>
+                            deleteCourse(
+                              selectedIcon,
+                              setError,
+                              setErrorMessage,
+                              setNotifType,
+                              error,
+                              token,
+                              setFilteredData,
+                              filteredData
+                            )
+                          }
+                          handleClose={() => setDisplayBtns(false)}
+                          handleEdit={() =>
+                            history.push(`/edit-course/${row.id}`)
+                          }
+                        />
+                      </td>
+                    </tr>
+                  ))}
+              </tbody>
+            </S.Table>
+            {filteredData && (
+              <S.CoursesCount>
+                Total Courses: {filteredData.length}
+              </S.CoursesCount>
+            )}
+          </S.FlexDiv>
+        ) : (
+          <Loading />
+        )}
+      </S.TableBox>
+    </MainWrapper>
+  );
+}
+
+export default ViewCourses;
---
+++ src/pages/ViewCourses/ViewCourses.style.js
@@ -0,0 +1,117 @@
+import styled from 'styled-components';
+
+export const TableBox = styled.div`
+  padding: 2em;
+  box-sizing: border-box;
+  max-height: 75vh;
+  overflow-y: auto;
+  @media (max-width: ${({ theme }) => theme.smallLaptop}) {
+    padding: 1.2em;
+  }
+  @media (max-width: ${({ theme }) => theme.tablet}) {
+    max-height: 88vh;
+  }
+  @media (max-width: ${({ theme }) => theme.mobile}) {
+    font-size: 1.4em;
+    && table,
+    thead,
+    tbody,
+    th,
+    td,
+    tr {
+      display: block;
+    }
+    && thead tr {
+      position: absolute;
+      top: -9999px;
+      left: -9999px;
+    }
+    && tr {
+      border: 1px solid #ccc;
+    }
+    && td {
+      /* Behave  like a "row" */
+      border: none;
+      border-bottom: 1px solid #eee;
+      position: relative;
+      padding-left: 50% !important;
+    }
+    && td:before {
+      /* Now like a table header */
+      position: absolute;
+      /* Top/left values mimic padding */
+      top: 6px;
+      left: 6px;
+      width: 45%;
+      padding-right: 10px;
+      white-space: nowrap;
+    }
+    && td:nth-of-type(1):before {
+      content: 'COURSE TITLE';
+    }
+    && td:nth-of-type(2):before {
+      content: 'LECTURER';
+    }
+    && td:nth-of-type(3):before {
+      content: 'DESCRIPTION';
+    }
+    && td:nth-of-type(4):before {
+      content: 'STUDENTS IN COURSE';
+    }
+    && td:nth-of-type(5):before {
+      content: 'CREATED';
+    }
+    && td:nth-of-type(6):before {
+      content: 'ACTION';
+    }
+  }
+`;
+export const Title = styled.h2`
+  font-weight: 400;
+  font-size: 1.7em;
+`;
+
+export const Table = styled.table`
+  border-collapse: separate;
+  border-spacing: 0;
+  color: ${(props) => props.theme.primary.color};
+  border-radius: 5px;
+  width: 100%;
+  text-align: left;
+  table-layout: fixed;
+  overflow: hidden;
+  background-color: #fff;
+  margin-top: 5px;
+
+  box-shadow: 1px 3px 3px 1px ${(props) => props.theme.primaryHover.background};
+  && th {
+    border-bottom: 1px solid #999;
+    padding: 0.5em 1em;
+  }
+  && td {
+    border-top: 1px solid #999;
+
+    padding: 0.5em 1em;
+    word-wrap: break-word;
+  }
+  && td:first-child {
+    color: ${(props) => props.theme.secondary.color};
+    font-weight: 500;
+  }
+`;
+
+export const FlexDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+`;
+export const CoursesCount = styled.div`
+  position: absolute;
+  bottom: 5%;
+  color: ${(props) => props.theme.primary.color};
+  font-size: 0.9em;
+  font-weight: 500;
+  @media (max-width: ${({ theme }) => theme.tablet}) {
+    bottom: 2%;
+  }
+`;
