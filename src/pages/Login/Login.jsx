cb1670678b81a8ead0f7d535323d6419ec0f1f94
MindaugasSak <m.sakenis@gmail.com>
2020-12-20 11:06:09 +0200

    fix for testing

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -17,7 +17,7 @@ function loginTo(
 ) {
   error = false;
   setLoadingBtn(true);
-  fetch(`${process.env.REACT_APP_SERVER_URL}/api/Login`, {
+  fetch(`/api/Login`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
