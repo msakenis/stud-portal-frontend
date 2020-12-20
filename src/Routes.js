4bfc78bbdb5b73437767a9428c9d442d8a6c44ea
MindaugasSak <m.sakenis@gmail.com>
2020-12-20 10:01:36 +0200

    full router

--- src/Routes.js
+++ src/Routes.js
@@ -7,8 +7,11 @@ const Loginlazy = lazy(() => import('./pages/Login/Login'));
 const ViewStudentslazy = lazy(() =>
   import('./pages/ViewStudents/ViewStudents')
 );
+const ViewCourseslazy = lazy(() => import('./pages/ViewCourses/ViewCourses'));
 const AddStudentslazy = lazy(() => import('./pages/AddStudent/AddStudent'));
+const AddCourselazy = lazy(() => import('./pages/AddCourse/AddCourse'));
 const EditStudentslazy = lazy(() => import('./pages/EditStudent/EditStudent'));
+const EditCourselazy = lazy(() => import('./pages/EditCourse/EditCourse'));
 function Routes() {
   return (
     <Router>
@@ -17,11 +20,18 @@ function Routes() {
           <Route exact path="/login" component={Loginlazy} />
           <PrivateRoute exact path="/" component={Homelazy} />
           <PrivateRoute exact path="/students" component={ViewStudentslazy} />
+          <PrivateRoute exact path="/courses" component={ViewCourseslazy} />
           <PrivateRoute exact path="/add-student" component={AddStudentslazy} />
+          <PrivateRoute exact path="/add-course" component={AddCourselazy} />
           <PrivateRoute
             exact
             path="/edit-student/:id"
             component={EditStudentslazy}
+          />
+          <PrivateRoute
+            exact
+            path="/edit-course/:id"
+            component={EditCourselazy}
           />
         </Switch>
       </Suspense>
